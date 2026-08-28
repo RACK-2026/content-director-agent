d273e4db6493dda76dcb4e1a186829822c970c31
RACK-2026 <2796690649@qq.com>
2026-08-28 17:49:11 +0800

    Add content_director/__init__.py

---
+++ content_director/__init__.py
@@ -0,0 +1,8 @@
+"""Generic content scripting and review primitives."""
+
+from .models import Brief, Review, Script
+from .workflow import create_script, revise_script, review_script
+
+__all__ = ["Brief", "Review", "Script", "create_script", "revise_script", "review_script"]
+
+
